8be7706864826c9ac6672786c1827a0665b009d3
Oliver Elliott <oliver.elliott@acronis.com>
2025-12-02 12:10:43 +0000

    lets go

---
+++ src/day01/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    with open("src/day01/input.txt", "r") as f:
+        data = f.read().strip()
+    print(f"Input data: {data}")
+
+if __name__ == "__main__":
+    main()
